755e74a3f0e65c9bc4419cd9b31cd5357ec742d5
KudaTerbang1 <krepp130@gmail.com>
2022-06-05 07:56:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const gd = require("gamedig");
+const config = require("./config.json");
+
+const app = express();
+
+const port = config.port;
+
+app.get("/", (req, res) => {
+    const game = req.query.game;
+    const ip = req.query.ip;
+    const port = req.query.port;
+
+    gd.query({
+        type: game,
+        host: ip,
+        port: port,
+    }).then(jawab => {
+        res.send(jawab);
+    }).catch(error => {
+        res.send({
+            status: 404,
+            error: error.message
+        });
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Ready on port " + port);
+})
